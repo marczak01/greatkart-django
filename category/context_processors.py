8898641a73e0100cba0727a57c531efb97663f11
Marek Marczak <marczak01@icloud.com>
2022-02-13 18:29:24 +0100

    first commit

---
+++ category/context_processors.py
@@ -0,0 +1,7 @@
+from category.models import Category
+
+#function which is putting all category names into the list
+#called links. We use python dict() fnc to convert it into list
+def menu_links(request):
+    links = Category.objects.all()
+    return dict(links=links)
---
+++ store/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Product
+from category.models import Category
+# Create your views here.
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        #if we define url path like: "/store/shoes/"
+        categories = get_object_or_404(Category, slug=category_slug)
+        #if we try to get slug witch is None then we get 404 error
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        #if we do not define url path like "/store/shoes/"
+        #and we only say "/store/" then we'll see all available products
+        products = Product.objects.all().filter(is_available=True)
+        #only products with is_available set to True will be displayed
+        product_count = products.count()
+
+    category = Category.objects.all()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+        'category': category,
+    }
+
+    return render(request, 'store/store.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
